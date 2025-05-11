3902fa5ef5f837602401c682d688414213cb4964
Erfan <erfankhakha@gmail.com>
2025-05-11 19:35:18 +0330

    adding rays

--- sketch.js
+++ sketch.js
@@ -5,6 +5,10 @@ const MAP_NUM_COLS = 15;
 const WINDOW_WIDTH = MAP_NUM_COLS * TILE_SIZE;
 const WINDOW_HEIGHT = MAP_NUM_ROWS * TILE_SIZE; 
 
+const FOV_ANGLE = 60 * (Math.PI / 180);
+const WALL_STRIP_WIDTH = 30;
+const NUM_RAYS = WINDOW_WIDTH / WALL_STRIP_WIDTH;
+
 class Map {
   constructor() {
     this.grid = [
@@ -37,7 +41,7 @@ class Map {
 
   isNotInWalls(x, y) {
     if (x < 0 || x > WINDOW_WIDTH || y < 0 || y > WINDOW_HEIGHT) 
-      return;
+      return true;
 
     var gridX = Math.floor(x / TILE_SIZE);
     var gridY = Math.floor(y / TILE_SIZE);
@@ -58,7 +62,6 @@ class Player {
   }
 
   render() {
-    noStroke();
     fill("red");
     circle(this.x, this.y, this.radius);
     stroke("red");
@@ -67,7 +70,7 @@ class Player {
       this.y,
       this.x + Math.cos(this.rotationAngle) * 30,
       this.y + Math.sin(this.rotationAngle) * 30
-    )
+    );
   }
 
   update() {
@@ -84,8 +87,24 @@ class Player {
   }
 }
 
+class Ray {
+  constructor(rayAngle) {
+    this.rayAngle = rayAngle;    
+  }
+  render() {
+    stroke("rgba(0, 0, 255, 0.3)");
+    line(
+      player.x,
+      player.y,
+      player.x + Math.cos(this.rayAngle) * 30,
+      player.y + Math.sin(this.rayAngle) * 30
+      );
+  }
+}
+
 var grid = new Map();
 var player = new Player();
+var rays = [];
 
 function keyPressed() {
   if (keyCode == UP_ARROW) {
@@ -119,17 +138,38 @@ function keyReleased() {
   }
 }
 
+function castAllRays() {
+  var columsId = 0;
+
+  // starting ray position by subtracting half of the FOV
+  var rayAngle = player.rotationAngle - FOV_ANGLE / 2;
+
+  rays = [];
+  for (var i = 0; i < 1 /* NUM_RAYS */; i++) {
+    var ray = new Ray(rayAngle);
+    // ray.cast();
+    rays.push(ray);
+
+    rayAngle += FOV_ANGLE / NUM_RAYS;
+    columsId ++;
+  }
+}
+
 function setup() {
   createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
 }
 
 function update() {
     player.update();
+    castAllRays();
 }
 
 function draw() {
   update();
   background(400);
   grid.render();
+  for (ray of rays) {
+    ray.render();
+  }
   player.render();
 }
